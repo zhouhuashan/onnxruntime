4c8ccf00c277ada1c8a218b7b5c55d1616b34a69
George Wu <jywu@microsoft.com>
2017-10-09 21:41:04 +0000

    Merged PR 270: ensure models dir doesn't exist before clone
    Ensure models dir doesn't exist before clone. This is needed because we are using custom DOWNLOAD_COMMAND to enable shallow clone.
    
    If we were using CMake 3.6 or later, we could use GIT_SHALLOW 1 option. Unfortunately, our vsts build installs cmake via nuget and the latest version published is 3.5.2

--- cmake/external/models.cmake
+++ cmake/external/models.cmake
@@ -2,13 +2,14 @@ include (ExternalProject)
 
 set(models models)
 set(models_URL  https://aiinfra.visualstudio.com/Lotus/_git/models)
+set(models_dir ${CMAKE_CURRENT_BINARY_DIR}/models/models)
 
 ExternalProject_Add(models
     PREFIX ${models}
     GIT_REPOSITORY ${models_URL}
     DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}/models
-    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/models/models
-    DOWNLOAD_COMMAND git clone --depth 1 ${models_URL}
+    SOURCE_DIR ${models_dir}
+    DOWNLOAD_COMMAND ${CMAKE_COMMAND} -E remove_directory ${models_dir} && git clone --depth 1 ${models_URL}
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
