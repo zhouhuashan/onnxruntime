28d1ed921262bf8c7e7fdd644da20c0fec258afd
Edward Chen <edch@microsoft.com>
2017-10-11 23:40:57 +0000

    Merged PR 295: Removed Python dependency, added patch for protobuf.
    Removed Python dependency, added patch for protobuf.

--- cmake/lotusIR_util.cmake
+++ cmake/lotusIR_util.cmake
@@ -21,8 +21,6 @@ function(RELATIVE_PROTOBUF_GENERATE_CPP SRCS HDRS ROOT_DIR)
              "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h"
       COMMAND ${PROTOBUF_PROTOC_EXECUTABLE}
       ARGS --cpp_out  ${CMAKE_CURRENT_BINARY_DIR} -I ${ROOT_DIR} ${ABS_FIL} -I ${PROTOBUF_INCLUDE_DIRS}
-      COMMAND ${PYTHON3_EXECUTABLE}
-      ARGS ${LOTUSIR_ROOT}/tools/wrap_proto_header.py ${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h
       DEPENDS ${ABS_FIL} protobuf
       COMMENT "Running C++ protocol buffer compiler on ${FIL}"
       VERBATIM )
