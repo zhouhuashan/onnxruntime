4838e38e5d39ead863604afda2179bac9eabe2ef
Edward Chen <edch@microsoft.com>
2017-10-13 20:31:04 +0000

    Merged PR 312: Merge edch/get_cmake_windows to master
    LotusIR: Added script to download more recent CMake version for VSTS Windows build. Updated CMake version in VSTS Linux build.

---
+++ tools/ci_build/vsts/windows/get_cmake.ps1
@@ -0,0 +1,43 @@
+# downloads CMake and puts it in the given output directory
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$outputDirectory
+)
+
+$ErrorActionPreference = "Stop"
+
+$cmakeDirectory = $(Join-Path $outputDirectory "cmake")
+
+# create temporary directory
+$tempDir = $(Join-Path $pwd "get_cmake.tmp")
+New-Item -Path $tempDir -ItemType "directory" | Out-Null
+
+try
+{
+    # download CMake
+    Write-Host "Downloading CMake..."
+    $cmakeUrl = "https://cmake.org/files/v3.9/cmake-3.9.4-win64-x64.zip"
+    Invoke-WebRequest -Uri $cmakeUrl -OutFile $(Join-Path $tempDir "cmake.zip")
+
+    # extract cmake.zip
+    Write-Host "Extracting CMake..."
+    Add-Type -Assembly "System.IO.Compression.FileSystem"
+    [System.IO.Compression.ZipFile]::ExtractToDirectory(
+        $(Join-Path $tempDir "cmake.zip"),
+        $(Join-Path $tempDir "cmake"))
+
+    # clean existing directory, if any
+    if (Test-Path -LiteralPath $cmakeDirectory -PathType Container)
+    {
+        Remove-Item -LiteralPath $cmakeDirectory -Recurse -Force
+    }
+
+    # move extracted files
+    $extractedCmakeDirectory = $(Join-Path $(Join-Path $tempDir "cmake") "cmake*" -Resolve)
+    Move-Item $extractedCmakeDirectory $cmakeDirectory
+}
+finally
+{
+    # clean up temporary directory
+    Remove-Item $tempDir -Recurse
+}
