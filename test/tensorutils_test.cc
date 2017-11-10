f300751bf575f4f1aa2f8afb64c7f22516db407a
Cheng Tang <chenta@microsoft.com>
2017-11-10 11:08:56 -0800

    update unpack tensor signature
    use int64
    
    fix linux build break
    
    fix comments in cr
    
    rename parameter and signature
    
    fix merge conflict for onnx

--- test/tensorutils_test.cc
+++ test/tensorutils_test.cc
@@ -15,14 +15,13 @@ namespace Lotus
             boolTensorProto.set_data_type(TensorProto_DataType_BOOL);
             boolTensorProto.add_int32_data(1);
 
-            std::vector<bool> boolData;
-            auto status = TensorUtils::UnpackTensor(boolTensorProto, &boolData);
+            bool boolData[1];
+            auto status = TensorUtils::UnpackTensor(boolTensorProto, boolData, 1);
             EXPECT_TRUE(status.Ok());
-            EXPECT_EQ(1, boolData.size());
             EXPECT_TRUE(boolData[0]);
 
-            std::vector<float> floatData;
-            status = TensorUtils::UnpackTensor(boolTensorProto, &floatData);
+            float floatData[1];
+            status = TensorUtils::UnpackTensor(boolTensorProto, floatData, 1);
             EXPECT_FALSE(status.Ok());
 
             TensorProto floatTensorProto;
@@ -36,26 +35,26 @@ namespace Lotus
 
             rawdata[sizeof(float) * 4] = '\0';
             floatTensorProto.set_raw_data(rawdata);
-            TensorUtils::UnpackTensor(floatTensorProto, &floatData);
-            EXPECT_EQ(4, floatData.size());
-            EXPECT_EQ(1.1f, floatData[0]);
-            EXPECT_EQ(2.2f, floatData[1]);
-            EXPECT_EQ(3.3f, floatData[2]);
-            EXPECT_EQ(4.4f, floatData[3]);
+            float floatData2[4];
+            status = TensorUtils::UnpackTensor(floatTensorProto, floatData2, 4);
+            EXPECT_TRUE(status.Ok());
+            EXPECT_EQ(1.1f, floatData2[0]);
+            EXPECT_EQ(2.2f, floatData2[1]);
+            EXPECT_EQ(3.3f, floatData2[2]);
+            EXPECT_EQ(4.4f, floatData2[3]);
 
             TensorProto stringTensorProto;
             stringTensorProto.set_data_type(TensorProto_DataType_STRING);
             stringTensorProto.add_string_data("a");
             stringTensorProto.add_string_data("b");
 
-            std::vector<std::string> stringData;
-            status = TensorUtils::UnpackTensor(stringTensorProto, &stringData);
+            std::string stringData[2];
+            status = TensorUtils::UnpackTensor(stringTensorProto, stringData, 2);
             EXPECT_TRUE(status.Ok());
-            EXPECT_EQ(2, stringData.size());
             EXPECT_EQ("a", stringData[0]);
             EXPECT_EQ("b", stringData[1]);
 
-            status = TensorUtils::UnpackTensor(boolTensorProto, &stringData);
+            status = TensorUtils::UnpackTensor(boolTensorProto, stringData, 2);
             EXPECT_FALSE(status.Ok());
         }
     }
