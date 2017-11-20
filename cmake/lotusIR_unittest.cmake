cced95a704f6aa48326032ce57c4a0ce451250e5
George Wu <jywu@microsoft.com>
2017-11-20 22:38:46 +0000

    Merged PR 546: update wholearchive option for lotusIR unit test to be compatible with VS2017
    Update wholearchive option for lotusIR unit test to be compatible with VS2017 agent.
    This matches what was done in LotusRT.

--- cmake/lotusIR_unittest.cmake
+++ cmake/lotusIR_unittest.cmake
@@ -7,10 +7,10 @@ function(add_whole_archive_flag lib output_var)
     set(${output_var} -Wl,-force_load,$<TARGET_FILE:${lib}> PARENT_SCOPE)
   elseif(MSVC)
     # In MSVC, we will add whole archive in default.
-    set(${output_var} -WHOLEARCHIVE:$<TARGET_FILE:${lib}> PARENT_SCOPE)
+    set(${output_var} -WHOLEARCHIVE:$<SHELL_PATH:$<TARGET_FILE:${lib}>> PARENT_SCOPE)
   else()
     # Assume everything else is like gcc
-    set(${output_var} -Wl,--whole-archive ${lib} -Wl,--no-whole-archive PARENT_SCOPE)
+    set(${output_var} "-Wl,--whole-archive $<TARGET_FILE:${lib}> -Wl,--no-whole-archive" PARENT_SCOPE)
   endif()
 endfunction()
 
