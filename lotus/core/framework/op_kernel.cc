a018d344cdc74b1fa790fe8a228f36c76a7880b3
Ke Zhang <kezhan@microsoft.com>
2018-03-02 21:20:45 +0000

    Merged PR 972: adding one interface to OpKernelContext for output not allocated.
    adding one interface to OpKernelContext for output having been allocated.

--- lotus/core/framework/op_kernel.cc
+++ lotus/core/framework/op_kernel.cc
@@ -2,10 +2,10 @@
 
 namespace Lotus {
 
-// TODO dummy implementation to facilitate testing
-AllocatorInfo* OpKernelInfo::GetAllocatorInfo() {
-  return nullptr;
-}
+    // TODO dummy implementation to facilitate testing
+    AllocatorInfo* OpKernelInfo::GetAllocatorInfo() {
+        return nullptr;
+    }
 
 #define DEFINE_GET_ATTR(T, type)                                                  \
   template <>                                                                     \
@@ -44,22 +44,22 @@ AllocatorInfo* OpKernelInfo::GetAllocatorInfo() {
     return Status(LOTUS, FAIL, "No attribute with this name is defined.");        \
   }
 
-  DEFINE_GET_ATTR(float, f)
-  DEFINE_GET_ATTR(int, i)
-  DEFINE_GET_ATTR(std::string, s)
-  DEFINE_GET_ATTR(TensorProto, t)
-  DEFINE_GET_ATTR(GraphProto, g)
-  DEFINE_GET_ATTRS(float, floats)
-  DEFINE_GET_ATTRS(int, ints)
-  DEFINE_GET_ATTRS(std::string, strings)
-  DEFINE_GET_ATTRS(TensorProto, tensors)
-  DEFINE_GET_ATTRS(GraphProto, graphs)
-  
-  template<typename T>
-  T* OpKernelContext::Output(int index) const
-  {
-    int num_inputs = m_kernel->num_inputs();
-    ExecutionFrame::NodeArgValue value = m_arg_start[num_inputs + index];
-    return reinterpret_cast<T*>(value->pData);
-  }
+    DEFINE_GET_ATTR(float, f)
+    DEFINE_GET_ATTR(int, i)
+    DEFINE_GET_ATTR(std::string, s)
+    DEFINE_GET_ATTR(TensorProto, t)
+    DEFINE_GET_ATTR(GraphProto, g)
+    DEFINE_GET_ATTRS(float, floats)
+    DEFINE_GET_ATTRS(int, ints)
+    DEFINE_GET_ATTRS(std::string, strings)
+    DEFINE_GET_ATTRS(TensorProto, tensors)
+    DEFINE_GET_ATTRS(GraphProto, graphs)
+
+    Tensor* OpKernelContext::Output(int index, const TensorShape& shape)
+    {
+        // In this case, it's assumed that the tensor hasn't been allocated yet,
+        // so that it's calling ExecutionFrame to create a tensor in the given position with given shape.
+        auto output_arg_index = arg_start_index + static_cast<int>(m_kernel->num_inputs()) + index;
+        return m_execution_frame->GetOrCreateTensor(output_arg_index, shape);
+    }
 }
--- lotus/core/framework/op_kernel.h
+++ lotus/core/framework/op_kernel.h
@@ -11,106 +11,114 @@
 
 namespace Lotus {
 
-  class OpKernel;
-  
-  class OpKernelInfo
-  {
-  public:
-    explicit OpKernelInfo(const LotusIR::Node& node) : m_node(node) {}
+    class OpKernelContext;
 
-    ~OpKernelInfo();
-
-    template<typename T>
-    Status GetAttr(const std::string& name, T* value) const;
-
-    template<typename T>
-    Status GetAttr(const std::string& name, std::vector<T>& values) const;
-            
-    const LotusIR::Node& OpDef() const { return m_node; }
-
-    AllocatorInfo* GetAllocatorInfo();
-
-  private:
-    const LotusIR::Node& m_node;
-  };
-
-  class OpKernelContext {
-  public:
-    typedef std::unordered_map<std::string, size_t> ArgMap;
-    
-    explicit OpKernelContext(const LotusIR::Node& node, OpKernel* kernel,
-                             ExecutionFrame* frame)
-      : m_kernel(kernel),
-        m_execution_frame(frame)
+    class OpKernelInfo
     {
-      m_arg_start = frame->get_first_arg(node);
-    }
-    
-    ~OpKernelContext() {};
+    public:
+        explicit OpKernelInfo(const LotusIR::Node& node) : m_node(node) {}
 
-    template<typename T>
-    const T* Input(int index) const {
-      ExecutionFrame::NodeArgValue value = m_arg_start[index];
-      return reinterpret_cast<T*>(value->pData);
-    }
+        ~OpKernelInfo();
 
-    template<typename T>
-    T* Output(int index) const;
-      
-  private:
-    ExecutionFrame* m_execution_frame = nullptr;
-    OpKernel* m_kernel = nullptr;
-    ExecutionFrame::NodeArgValue* m_arg_start = nullptr;
-  };
+        template<typename T>
+        Status GetAttr(const std::string& name, T* value) const;
 
-  class OpKernel {
-  public:
-    typedef std::function<void()> DoneCallback;
+        template<typename T>
+        Status GetAttr(const std::string& name, std::vector<T>& values) const;
 
-    explicit OpKernel(OpKernelInfo* info)
-      : m_node(info->OpDef()),
-        m_alloc(info->GetAllocatorInfo()) {
-      m_input_start_index.resize(m_node.InputArgCount().size());
-      size_t index = 0;
-      for (size_t i = 0; i < m_input_start_index.size(); i++) {
-        m_input_start_index[i] = index;
-        index += m_node.InputArgCount()[i];
-      }
-    }
+        const LotusIR::Node& OpDef() const { return m_node; }
 
-    // The total number of inputs.
-    size_t num_inputs() const
-    {
-      return m_node.InputDefs().size();
-    }
+        AllocatorInfo* GetAllocatorInfo();
 
-    // The total number of outputs.    
-    size_t num_outputs() const
-    {
-      return m_node.OutputDefs().size();
-    }
-    
-    // Starting index for the i-th input argument.
-    size_t input_start_index(int arg_index) const
-    {
-      return m_input_start_index[arg_index];
-    }
+    private:
+        const LotusIR::Node& m_node;
+    };
 
-    // The number of inputs for the i-th input argument.
-    size_t input_size(int arg_index) const
-    {
-      return m_node.InputArgCount()[arg_index];
-    }
-    
-    virtual void Compute(OpKernelContext* context) = 0;
-    virtual void ComputeAsync(OpKernelContext* context, DoneCallback done) = 0;
+    class OpKernel {
+    public:
+        typedef std::function<void()> DoneCallback;
 
-    const AllocatorInfo& allocator() { return *m_alloc; }
+        explicit OpKernel(OpKernelInfo* info)
+            : m_node(info->OpDef()),
+            m_alloc(info->GetAllocatorInfo()) {
+            m_input_start_index.resize(m_node.InputArgCount().size());
+            size_t index = 0;
+            for (size_t i = 0; i < m_input_start_index.size(); i++) {
+                m_input_start_index[i] = index;
+                index += m_node.InputArgCount()[i];
+            }
+        }
 
-  private:
-    AllocatorInfo* m_alloc;
-    const LotusIR::Node& m_node;
-    std::vector<size_t> m_input_start_index;
-  };
+        // The total number of inputs.
+        size_t num_inputs() const
+        {
+            return m_node.InputDefs().size();
+        }
+
+        // The total number of outputs.    
+        size_t num_outputs() const
+        {
+            return m_node.OutputDefs().size();
+        }
+
+        // Starting index for the i-th input argument.
+        size_t input_start_index(int arg_index) const
+        {
+            return m_input_start_index[arg_index];
+        }
+
+        // The number of inputs for the i-th input argument.
+        size_t input_size(int arg_index) const
+        {
+            return m_node.InputArgCount()[arg_index];
+        }
+
+        virtual void Compute(OpKernelContext* context) = 0;
+        virtual void ComputeAsync(OpKernelContext* context, DoneCallback done) = 0;
+
+        const AllocatorInfo& allocator() { return *m_alloc; }
+
+    private:
+        AllocatorInfo* m_alloc;
+        const LotusIR::Node& m_node;
+        std::vector<size_t> m_input_start_index;
+    };
+
+    class OpKernelContext {
+    public:
+        typedef std::unordered_map<std::string, size_t> ArgMap;
+
+        explicit OpKernelContext(const LotusIR::Node& node, OpKernel* kernel,
+            ExecutionFrame* frame)
+            : m_kernel(kernel),
+            m_execution_frame(frame)
+        {
+            arg_start_index = frame->get_first_arg(node);
+        }
+
+        ~OpKernelContext() {};
+
+        template<typename T>
+        const T* Input(int index) const {
+            return m_execution_frame->GetInput<T>(arg_start_index + index);
+        }
+
+        template<typename T>
+        T* Output(int index) {
+            auto output_arg_index = arg_start_index + static_cast<int>(m_kernel->num_inputs()) + index;
+            return m_execution_frame->GetOutput<T>(output_arg_index);
+        }
+
+        // In the case that memory allocation has not been done for an output tensor,
+        // The memory allocation will be done on-the-fly with given tensor shape.
+        Tensor* Output(int index, const TensorShape& shape);
+
+    private:
+        ExecutionFrame* m_execution_frame = nullptr;
+        OpKernel* m_kernel = nullptr;
+
+        // The argument starting index in ExecutionFrame.
+        int arg_start_index = -1;
+    };
 }
 #endif  // CORE_FRAMEWORK_OP_KERNEL_H
