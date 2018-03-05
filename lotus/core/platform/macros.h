8bb70a89825fdbcf16f32d5bfa98824c98d96dbd
Scott McKay <scmckay@microsoft.com>
2018-03-06 08:20:26 +1000

    Update macros.h Add clang-format and clang-tidy config files.

--- lotus/core/platform/macros.h
+++ lotus/core/platform/macros.h
@@ -1,25 +1,22 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 #ifndef LOTUS_PLATFORM_MACROS_H_
 #define LOTUS_PLATFORM_MACROS_H_
 
 // A macro to disallow the copy constructor and operator= functions
 // This is usually placed in the private: declarations for a class.
 #define LOTUS_DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&) = delete;         \
-  void operator=(const TypeName&) = delete
+    TypeName(const TypeName &) = delete;         \
+    TypeName &operator=(const TypeName &) = delete
+
+// A macro to disallow move construction and assignment
+// This is usually placed in the private: declarations for a class.
+#define LOTUS_DISALLOW_MOVE(TypeName)     \
+    TypeName(const TypeName &&) = delete; \
+    TypeName &operator=(const TypeName &&) = delete
+
+// A macro to disallow the copy constructor, operator= functions, move construction and move assignment
+// This is usually placed in the private: declarations for a class.
+#define LOTUS_DISALLOW_COPY_ASSIGN_AND_MOVE(TypeName) \
+    LOTUS_DISALLOW_COPY_AND_ASSIGN(TypeName);         \
+    LOTUS_DISALLOW_MOVE(TypeName)
 
 #endif  // LOTUS_PLATFORM_MACROS_H_
