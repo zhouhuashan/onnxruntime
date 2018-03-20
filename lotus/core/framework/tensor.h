0d73fe9e6cf4e860426fb464ada76e4c723462ac
Du Li <duli1@microsoft.com>
2018-03-20 17:08:09 +0000

    Merged PR 1026: Adding reshape kernel

--- lotus/core/framework/tensor.h
+++ lotus/core/framework/tensor.h
@@ -22,13 +22,7 @@ namespace Lotus
         TensorShape(const TensorShape& p_other);
 
         // Return the dimension specified by <p_idx>.
-        int64_t& operator[](int p_idx);
-
-        // Return the dimension specified by <p_idx>.
-        const int64_t& operator[](int p_idx) const;
-
-        // Return the number of dimensions
-        size_t DimensionCount() const { return m_dims.size(); }
+        const int64_t operator[](int p_idx) const;
 
         bool operator==(const TensorShape& p_other) const {
             return m_dims == p_other.m_dims;
@@ -38,7 +32,7 @@ namespace Lotus
             return !(*this == p_other);
         }
 
-        int64_t NumDimensions() const {
+        const int64_t NumDimensions() const {
             return m_dims.size();
         }
 
