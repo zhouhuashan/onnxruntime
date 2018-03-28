b67767e76dc93f4911aa2bce568560aad82c8ba6
Du Li <duli1@microsoft.com>
2018-03-28 00:06:03 +0000

    Merged PR 1131: disable non-copying reshape
    Disable non-copying reshape for now. Will revisit it once the memory planner is ready for alias

--- lotus/core/providers/cpu/tensor/reshape.cc
+++ lotus/core/providers/cpu/tensor/reshape.cc
@@ -2,15 +2,6 @@
 
 namespace Lotus {
 
-//non-copying reshape kernel
-REGISTER_KERNEL(KernelDef("Reshape")
-                    .Domain(LotusIR::kOnnxDomain)
-                    .SinceVersion(1, 2)
-                    .Provider(LotusIR::kCpuExecutionProvider)
-                    .TypeConstraint("T", DataTypeImpl::GetTensorType<float>())
-                    .Alias(0, 0),
-                Reshape<float>);
-
 //copying reshape kernel
 REGISTER_KERNEL(KernelDef("Reshape")
                     .Domain(LotusIR::kOnnxDomain)
