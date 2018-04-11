981663dcda826afb92bd8d1014d89d15d9e29f15
Liqun Fu <liqun.fu@microsoft.com>
2018-04-11 20:56:24 +0000

    Merged PR 1248: add max_roi_pool op
    add max_roi_pool op

---
+++ lotus/core/providers/cpu/nn/roi_pool.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "core/common/common.h"
+#include "core/framework/op_kernel.h"
+#include "core/providers/cpu/nn/autopad_type.h"
+
+namespace Lotus {
+
+template <typename T>
+class RoiPool : public OpKernel {
+ public:
+  RoiPool(OpKernelInfo info) : OpKernel(info) {
+    std::vector<int64_t> pooled_shape;
+    info.GetAttrs<int64_t>("pooled_shape", pooled_shape);
+    LOTUS_ENFORCE(pooled_shape.size() == 2);
+
+    pooled_height_ = pooled_shape[0];
+    pooled_width_ = pooled_shape[1];
+    LOTUS_ENFORCE(pooled_height_ > 0);
+    LOTUS_ENFORCE(pooled_width_ > 0);
+
+    info.GetAttr<float>("spatial_scale", &spatial_scale_);
+    LOTUS_ENFORCE(spatial_scale_ > 0);
+  }
+
+  virtual ~RoiPool() = default;
+
+  Status Compute(OpKernelContext* context) const;
+
+ protected:
+  int64_t pooled_height_, pooled_width_;
+  float spatial_scale_ = 1.0f;
+};
+}  // namespace Lotus
