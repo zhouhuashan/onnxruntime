2dc5a7d36066544f4902dbeb33b6267550b86e20
Ke Zhang <kezhan@microsoft.com>
2018-04-13 02:20:17 +0000

    Merged PR 1268: remove legacypadding as it's not needed any more, coreml converter will be
    remove legacypadding as it's not needed any more, coreml converter will be updated to handle it correctly. reshape kernel should use same op name.

--- lotus/core/providers/cpu/tensor/reshape.cc
+++ lotus/core/providers/cpu/tensor/reshape.cc
@@ -9,7 +9,7 @@ REGISTER_KERNEL(KernelDefBuilder("Reshape")
                     .TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
                 Reshape<float>);
 
-REGISTER_KERNEL(KernelDefBuilder("Reshape-1")
+REGISTER_KERNEL(KernelDefBuilder("Reshape")
                     .Domain(LotusIR::kOnnxDomain)
                     .SinceVersion(1, 4)
                     .Provider(LotusIR::kCpuExecutionProvider)
