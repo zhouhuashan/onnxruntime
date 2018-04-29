6d6ba08e9420ee026ef6bbbd06a96e31aac938d0
Scott McKay <Scott.McKay@microsoft.com>
2018-04-29 19:57:40 +0000

    Merged PR 1425: Add LabelEncoder operator
    LabelEncoder and tests.
    
    Related work items: #378

---
+++ lotus/core/providers/cpu/ml/label_encoder.cc
@@ -0,0 +1,66 @@
+#include "core/providers/cpu/ml/label_encoder.h"
+#include <algorithm>
+#include <gsl/span>
+
+namespace Lotus {
+namespace ML {
+
+REGISTER_KERNEL(KernelDefBuilder("LabelEncoder")
+                    .Domain(LotusIR::kMLDomain)
+                    .SinceVersion(1)
+                    .Provider(LotusIR::kCpuExecutionProvider)
+                    .TypeConstraint("T1",
+                                    std::vector<MLDataType>{DataTypeImpl::GetTensorType<std::string>(),
+                                                            DataTypeImpl::GetTensorType<int64_t>()})
+                    .TypeConstraint("T2",
+                                    std::vector<MLDataType>{DataTypeImpl::GetTensorType<std::string>(),
+                                                            DataTypeImpl::GetTensorType<int64_t>()}),
+                LabelEncoder);
+
+Status LabelEncoder::Compute(OpKernelContext* context) const {
+  const Tensor& X = *context->Input<Tensor>(0);
+  const TensorShape& shape = X.Shape();
+  Tensor& Y = *context->Output(0, TensorShape(shape));
+
+  auto input_type = X.DataType();
+
+  if (input_type == DataTypeImpl::GetType<std::string>()) {
+    if (Y.DataType() != DataTypeImpl::GetType<int64_t>())
+      return Status(LOTUS, FAIL, "Input of tensor(string) must have output of tensor(int64)");
+
+    auto input = gsl::make_span(X.Data<std::string>(), shape.Size());
+    auto output = gsl::make_span(Y.MutableData<int64_t>(), shape.Size());
+    auto out = output.begin();
+
+    // map isn't going to change so get end() once instead of calling inside the for_each loop
+    const auto map_end = string_to_int_map_.end();
+
+    std::for_each(input.cbegin(), input.cend(),
+                  [&out, &map_end, this](const std::string& value) {
+                    auto map_to = string_to_int_map_.find(value);
+                    *out = map_to == map_end ? default_int_ : map_to->second;
+                    ++out;
+                  });
+  } else {
+    if (Y.DataType() != DataTypeImpl::GetType<std::string>())
+      return Status(LOTUS, FAIL, "Input of tensor(int64) must have output of tensor(string)");
+
+    auto input = gsl::make_span(X.Data<int64_t>(), shape.Size());
+    auto output = gsl::make_span(Y.MutableData<std::string>(), shape.Size());
+    auto out = output.begin();
+
+    const auto map_end = int_to_string_map_.end();
+
+    std::for_each(input.cbegin(), input.cend(),
+                  [&out, &map_end, this](const int64_t& value) {
+                    auto map_to = int_to_string_map_.find(value);
+                    *out = map_to == map_end ? default_string_ : map_to->second;
+                    ++out;
+                  });
+  }
+
+  return Status::OK();
+}
+
+}  // namespace ML
+}  // namespace Lotus
---
+++ lotus/core/providers/cpu/ml/label_encoder.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "core/common/common.h"
+#include "core/framework/op_kernel.h"
+#include "core/providers/cpu/ml/ml_common.h"
+
+namespace Lotus {
+namespace ML {
+
+class LabelEncoder final : public OpKernel {
+ public:
+  LabelEncoder(const OpKernelInfo& info) : OpKernel(info) {
+    std::vector<std::string> string_classes;
+
+    LOTUS_ENFORCE(op_kernel_info_.GetAttrs<std::string>("classes_strings", string_classes).IsOK());
+
+    LOTUS_ENFORCE(op_kernel_info_.GetAttr<std::string>("default_string", &default_string_).IsOK());
+    LOTUS_ENFORCE(op_kernel_info_.GetAttr<int64_t>("default_int64", &default_int_).IsOK());
+
+    auto num_entries = string_classes.size();
+
+    string_to_int_map_.reserve(num_entries);
+    int_to_string_map_.reserve(num_entries);
+
+    for (size_t i = 0; i < num_entries; ++i) {
+      const std::string& str = string_classes[i];
+
+      string_to_int_map_[str] = i;
+      int_to_string_map_[i] = str;
+    }
+  }
+
+  Status Compute(OpKernelContext* context) const override;
+
+ private:
+  std::unordered_map<std::string, int64_t> string_to_int_map_;
+  std::unordered_map<int64_t, std::string> int_to_string_map_;
+
+  std::string default_string_;
+  int64_t default_int_;
+};
+
+}  // namespace ML
+}  // namespace Lotus
