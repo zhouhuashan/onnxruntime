18767e8b23f4eda47135f72dd2d6f213d0136211
Ryan Hill <ryanhill@microsoft.com>
2018-05-11 02:18:12 +0000

    Merged PR 1518: Enable Memory leak detection
    A message box will appear at exit for any memory leaks, and the callstacks of each leak are dumped into the debug output window. A leak will cause the process to exit(-1) and fail the unit test.
    
    It won't catch leaks for code using malloc/free/realloc/etc unless they include the debug_alloc.h header.
    
    Related work items: #407

---
+++ lotus/core/platform/windows/debug_alloc.h
@@ -0,0 +1,10 @@
+#if defined(_DEBUG)
+void *DebugHeapAlloc(size_t size, unsigned framesToSkip = 0);
+void *DebugHeapReAlloc(void *p, size_t size);
+void DebugHeapFree(void *p) noexcept;
+
+#define calloc CallocNotImplemented
+#define malloc DebugHeapAlloc
+#define realloc DebugHeapReAlloc
+#define free DebugHeapFree
+#endif
