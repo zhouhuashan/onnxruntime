3edaf839386ac43b0fc9a6702149bdb7dc5e26fd
Liqun Fu <liqun.fu@microsoft.com>
2018-05-14 21:43:29 +0000

    Merged PR 1507: replease depth-space ops transpose code with Eigen call
    replease depth-space ops transpose code with Eigen call
    
    Related work items: #438

--- lotus/core/providers/cpu/tensor/space_depth_ops.cc
+++ lotus/core/providers/cpu/tensor/space_depth_ops.cc
@@ -1,5 +1,12 @@
 #include "core/providers/cpu/tensor/space_depth_ops.h"
 
+// warning raised from Eigen Tensor code.
+#ifdef _MSC_VER
+#pragma warning(disable : 4554)
+#endif
+#include "eigen/src/eigen/unsupported/Eigen/CXX11/Tensor"
+#include <array>
+
 namespace Lotus {
 
 REGISTER_KERNEL(KernelDefBuilder("SpaceToDepth")
@@ -15,6 +22,14 @@ REGISTER_KERNEL(KernelDefBuilder("DepthToSpace")
                     .Provider(LotusIR::kCpuExecutionProvider)
                     .TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
                 DepthToSpace<float>);
+
+// intemediate tensor shapes are:
+// (batch, blocksize, blocksize, input_depth / (blocksize * blocksize), input_height, input_width) for DepthToSpace
+// (batch, input_depth, input_height / blocksize, blocksize, input_width / blocksize, blocksize) for SpaceToDepth
+const int IntermediateTensorRank = 6;
+typedef Eigen::TensorMap<Eigen::Tensor<float, IntermediateTensorRank, Eigen::RowMajor, int64_t>,
+                         Eigen::Aligned>
+    EigenTensorMap;
 
 template <>
 Status SpaceToDepth<float>::Compute(OpKernelContext* context) const {
@@ -33,34 +48,13 @@ Status SpaceToDepth<float>::Compute(OpKernelContext* context) const {
   std::vector<int64_t> output_dims({batch, output_depth, output_height, output_width});
   Tensor& output = *context->Output(0, output_dims);
 
-  for (int64_t b = 0; b < batch; ++b) {
-    for (int64_t out_d = 0; out_d < output_depth; ++out_d) {
-      const int64_t in_d = out_d % input_depth;
-      const int64_t offset_w = (out_d / input_depth) % blocksize_;
-      const int64_t offset_h = (out_d / input_depth) / blocksize_;
-      for (int64_t out_h = 0; out_h < output_height; ++out_h) {
-        const int64_t in_h = out_h * blocksize_ + offset_h;
-        for (int64_t out_w = 0; out_w < output_width; ++out_w) {
-          const int64_t in_w = out_w * blocksize_ + offset_w;
-          const auto output_offset =
-              ((b * output_depth + out_d) * output_height + out_h) *
-                  output_width +
-              out_w;
-          const auto input_offset =
-              ((b * input_depth + in_d) * input_height + in_h) *
-                  input_width +
-              in_w;
-          if (in_h >= 0 && in_w >= 0 && in_h < input_height &&
-              in_w < input_width) {
-            output.template MutableData<float>()[output_offset] =
-                input.template Data<float>()[input_offset];
-          } else {
-            output.template MutableData<float>()[output_offset] = 0.0;
-          }
-        }
-      }
-    }
-  }
+  std::array<int64_t, IntermediateTensorRank> permutation{0, 3, 5, 1, 2, 4};
+  EigenTensorMap(output.MutableData<float>(), batch, blocksize_, blocksize_,
+                 input_depth, input_height / blocksize_, input_width / blocksize_) =
+      EigenTensorMap(const_cast<float*>(input.Data<float>()), batch,
+                     input_depth, input_height / blocksize_, blocksize_,
+                     input_width / blocksize_, blocksize_)
+          .shuffle(permutation);
 
   return Status::OK();
 }
@@ -83,32 +77,13 @@ Status DepthToSpace<float>::Compute(OpKernelContext* context) const {
   std::vector<int64_t> output_dims({batch, output_depth, output_height, output_width});
   Tensor& output = *context->Output(0, output_dims);
 
-  for (int64_t b = 0; b < batch; ++b) {
-    for (int64_t in_d = 0; in_d < input_depth; ++in_d) {
-      const int64_t out_d = in_d % output_depth;
-      const int64_t offset_w = (in_d / output_depth) % blocksize_;
-      const int64_t offset_h = (in_d / output_depth) / blocksize_;
-      for (int64_t in_h = 0; in_h < input_height; ++in_h) {
-        const int64_t out_h = in_h * blocksize_ + offset_h;
-        for (int64_t in_w = 0; in_w < input_width; ++in_w) {
-          const int64_t out_w = in_w * blocksize_ + offset_w;
-          if (out_h >= 0 && out_w >= 0 && out_h < output_height &&
-              out_w < output_width) {
-            const auto output_offset =
-                ((b * output_depth + out_d) * output_height + out_h) *
-                    output_width +
-                out_w;
-            const auto input_offset =
-                ((b * input_depth + in_d) * input_height + in_h) *
-                    input_width +
-                in_w;
-            output.template MutableData<float>()[output_offset] =
-                input.template Data<float>()[input_offset];
-          }
-        }
-      }
-    }
-  }
+  std::array<int64_t, IntermediateTensorRank> permutation{0, 3, 4, 1, 5, 2};
+  EigenTensorMap(output.MutableData<float>(), batch, input_depth / blocksize_ / blocksize_,
+                 input_height, blocksize_, input_width, blocksize_) =
+      EigenTensorMap(const_cast<float*>(input.Data<float>()), batch,
+                     blocksize_, blocksize_, input_depth / blocksize_ / blocksize_,
+                     input_height, input_width)
+          .shuffle(permutation);
 
   return Status::OK();
 }
