8848697c7831f7bc8b5a3a4a9bb11396a21a6f2c
George Wu <jywu@microsoft.com>
2018-05-25 22:12:32 +0000

    Merged PR 1645: more python tests.
    add some more python tests.
    
    bugfixes addressed by recent PR's
    https://aiinfra.visualstudio.com/_git/Lotus/pullrequest/1624?_a=overview
    https://aiinfra.visualstudio.com/_git/Lotus/pullrequest/1626?_a=overview
    
    allow these tests to pass.

--- lotus/test/python/lotus_test_python.py
+++ lotus/test/python/lotus_test_python.py
@@ -10,16 +10,50 @@ import lotus
 class TestInferenceSession(unittest.TestCase):
 
     def testRunModel(self):
-        m = lotus.InferenceSession("testdata/mul_1.pb")
+        sess = lotus.InferenceSession("testdata/mul_1.pb")
         input = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=np.float32)
-        input_name = m.get_inputs()[0].name
+        input_name = sess.get_inputs()[0].name
         self.assertEqual(input_name, "X")
-        shape = m.get_inputs()[0].shape
-        self.assertEqual(shape, [3, 2])
-        output_name = m.get_outputs()[0].name
+        input_shape = sess.get_inputs()[0].shape
+        self.assertEqual(input_shape, [3, 2])
+        output_name = sess.get_outputs()[0].name
         self.assertEqual(output_name, "Y")
-        res = m.run([output_name], {input_name: input})
+        output_shape = sess.get_outputs()[0].shape
+        self.assertEqual(output_shape, [3, 2])
+        res = sess.run([output_name], {input_name: input})
         output_expected = np.array([[1.0, 4.0], [9.0, 16.0], [25.0, 36.0]], dtype=np.float32)
+        np.testing.assert_allclose(output_expected, res[0], rtol=1e-05, atol=1e-08)
+
+    def testRunModel2(self):
+        sess = lotus.InferenceSession("testdata/matmul_1.pb")
+        input = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=np.float32)
+        input_name = sess.get_inputs()[0].name
+        self.assertEqual(input_name, "X")
+        input_shape = sess.get_inputs()[0].shape
+        self.assertEqual(input_shape, [3, 2])
+        output_name = sess.get_outputs()[0].name
+        self.assertEqual(output_name, "Y")
+        output_shape = sess.get_outputs()[0].shape
+        self.assertEqual(output_shape, [3, 1])
+        res = sess.run([output_name], {input_name: input})
+        output_expected = np.array([[5.0], [11.0], [17.0]], dtype=np.float32)
+        np.testing.assert_allclose(output_expected, res[0], rtol=1e-05, atol=1e-08)
+
+    def testRunModelSymbolicInput(self):
+        sess = lotus.InferenceSession("testdata/matmul_2.pb")
+        input = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=np.float32)
+        input_name = sess.get_inputs()[0].name
+        self.assertEqual(input_name, "X")
+        input_shape = sess.get_inputs()[0].shape
+        # Input X has an unknown dimension.
+        self.assertEqual(input_shape, [None, 2])
+        output_name = sess.get_outputs()[0].name
+        self.assertEqual(output_name, "Y")
+        output_shape = sess.get_outputs()[0].shape
+        # Output X has an unknown dimension.
+        self.assertEqual(output_shape, [None, 1])
+        res = sess.run([output_name], {input_name: input})
+        output_expected = np.array([[5.0], [11.0], [17.0]], dtype=np.float32)
         np.testing.assert_allclose(output_expected, res[0], rtol=1e-05, atol=1e-08)
 
 if __name__ == '__main__':
