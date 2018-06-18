8def0856700bbfd01d9ca2e6e428d6e77300663c
Jaliya Ekanayake <jaliyaek@microsoft.com>
2018-06-18 18:14:42 +0000

    Merged PR 1852: Adding int* to float cast support
    Related work items: #758

--- lotus/core/providers/cpu/tensor/cast_op.cc
+++ lotus/core/providers/cpu/tensor/cast_op.cc
@@ -1,4 +1,5 @@
 #include "core/providers/cpu/tensor/cast_op.h"
+#include <sstream>
 
 namespace Lotus {
 
@@ -10,6 +11,7 @@ const std::vector<MLDataType> castOpTypeConstraints{
     DataTypeImpl::GetTensorType<uint16_t>(),
     DataTypeImpl::GetTensorType<uint32_t>(),
     DataTypeImpl::GetTensorType<uint64_t>(),
+    DataTypeImpl::GetTensorType<int8_t>(),
     DataTypeImpl::GetTensorType<int16_t>(),
     DataTypeImpl::GetTensorType<int32_t>(),
     DataTypeImpl::GetTensorType<int64_t>(),
@@ -23,13 +25,33 @@ REGISTER_KERNEL(KernelDefBuilder("Cast")
                     .TypeConstraint("T2", castOpTypeConstraints),
                 Cast<float>);
 
-REGISTER_KERNEL(KernelDefBuilder("Cast")
-                    .Domain(LotusIR::kOnnxDomain)
-                    .SinceVersion(1)
-                    .Provider(LotusIR::kCpuExecutionProvider)
-                    .TypeConstraint("T1", DataTypeImpl::GetTensorType<MLFloat16>())
-                    .TypeConstraint("T2", DataTypeImpl::GetTensorType<float>()),
-                Cast<MLFloat16>);
+#define ADD_TO_FLOAT_CAST_OP(in_type)                                               \
+  REGISTER_KERNEL(KernelDefBuilder("Cast")                                          \
+                      .Domain(LotusIR::kOnnxDomain)                                 \
+                      .SinceVersion(1)                                              \
+                      .Provider(LotusIR::kCpuExecutionProvider)                     \
+                      .TypeConstraint("T1", DataTypeImpl::GetTensorType<in_type>()) \
+                      .TypeConstraint("T2", DataTypeImpl::GetTensorType<float>()),  \
+                  Cast<in_type>);                                                   \
+                                                                                    \
+template<>                                                                          \
+Status Cast<in_type>::Compute(OpKernelContext* context) const {                     \
+    const Tensor* X = context->Input<Tensor>(0);                                    \
+    const TensorShape& shape = X->Shape();                                          \
+    Tensor* Y = context->Output(0, TensorShape(shape));                             \
+    if (to_ != TensorProto_DataType_FLOAT)                                          \
+    /*todo: append the to_ type to string*/                                         \
+      return Status(LOTUS, FAIL, "Cast to unsupported type.");                      \
+    CastData<in_type, float>(X, Y, shape);                                          \
+    return Status::OK();                                                            \
+}
+
+ADD_TO_FLOAT_CAST_OP(uint8_t);
+ADD_TO_FLOAT_CAST_OP(int16_t);
+ADD_TO_FLOAT_CAST_OP(uint16_t);
+ADD_TO_FLOAT_CAST_OP(int32_t);
+ADD_TO_FLOAT_CAST_OP(uint32_t);
+ADD_TO_FLOAT_CAST_OP(MLFloat16);
 
 template <>
 Status Cast<float>::Compute(OpKernelContext* context) const {
@@ -68,33 +90,21 @@ Status Cast<float>::Compute(OpKernelContext* context) const {
     case TensorProto_DataType_DOUBLE:
       CastData<float, double>(X, Y, shape);
       break;
+    case TensorProto_DataType_FLOAT16:
+      CastData<float, MLFloat16>(X, Y, shape);
+      break;
     case TensorProto_DataType_INT8:
-      LOTUS_THROW("Casting to and from int8_t is not supported yet.");
+      LOTUS_THROW("Casting to and from int8 is not supported yet.");
       //break;
     case TensorProto_DataType_STRING:
       LOTUS_THROW("Casting to and from strings is not supported yet.");
       // break;
-    case TensorProto_DataType_FLOAT16:
-      CastData<float, MLFloat16>(X, Y, shape);
-      break;
     case TensorProto_DataType_UNDEFINED:
       LOTUS_THROW("Cast op must have 'to' argument of type DataType");
       // break;
     default:
       LOTUS_THROW("Unexpected 'to' argument value: ", to_);
   }
-  return Status::OK();
-}
-
-template <>
-Status Cast<MLFloat16>::Compute(OpKernelContext* context) const {
-  const Tensor* X = context->Input<Tensor>(0);
-  const TensorShape& shape = X->Shape();
-  Tensor* Y = context->Output(0, TensorShape(shape));
-  if (to_ != TensorProto_DataType_FLOAT)
-    //todo: append the to_ type to string
-    return Status(LOTUS, FAIL, "Cast from float16 to unsupported type.");
-  CastData<MLFloat16, float>(X, Y, shape);
   return Status::OK();
 }
 
