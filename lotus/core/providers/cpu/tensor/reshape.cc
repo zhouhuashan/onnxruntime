04f694b7dcbddf4851609c8ebe9339ae25d92351
Ke Deng <kedeng@microsoft.com>
2018-07-07 07:21:29 +0000

    Merged PR 1987: Register reshape for float16/float/double
    Register reshape for float16/float/double

--- lotus/core/providers/cpu/tensor/reshape.cc
+++ lotus/core/providers/cpu/tensor/reshape.cc
@@ -2,21 +2,26 @@
 
 namespace Lotus {
 
-REGISTER_KERNEL(KernelDefBuilder("Reshape")
-                    .Domain(LotusIR::kOnnxDomain)
-                    .SinceVersion(5)
-                    .Provider(LotusIR::kCpuExecutionProvider)
-                    .Alias(0, 0)
-                    .TypeConstraint("T", DataTypeImpl::GetTensorType<float>())
-                    .TypeConstraint("shape", DataTypeImpl::GetTensorType<int64_t>()),
-                Reshape<float>);
+#define REGISTER_KERNEL_TYPED(T)                                                        \
+  REGISTER_KERNEL(KernelDefBuilder("Reshape")                                           \
+                      .Domain(LotusIR::kOnnxDomain)                                     \
+                      .SinceVersion(5)                                                  \
+                      .Provider(LotusIR::kCpuExecutionProvider)                         \
+                      .Alias(0, 0)                                                      \
+                      .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())            \
+                      .TypeConstraint("shape", DataTypeImpl::GetTensorType<int64_t>()), \
+                  Reshape<T>);                                                          \
+                                                                                        \
+  REGISTER_KERNEL(KernelDefBuilder("Reshape")                                           \
+                      .Domain(LotusIR::kOnnxDomain)                                     \
+                      .SinceVersion(1, 4)                                               \
+                      .Provider(LotusIR::kCpuExecutionProvider)                         \
+                      .Alias(0, 0)                                                      \
+                      .TypeConstraint("T", DataTypeImpl::GetTensorType<T>()),           \
+                  Reshape_1<T>);
 
-REGISTER_KERNEL(KernelDefBuilder("Reshape")
-                    .Domain(LotusIR::kOnnxDomain)
-                    .SinceVersion(1, 4)
-                    .Provider(LotusIR::kCpuExecutionProvider)
-                    .Alias(0, 0)
-                    .TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
-                Reshape_1<float>);
+REGISTER_KERNEL_TYPED(MLFloat16)
+REGISTER_KERNEL_TYPED(float)
+REGISTER_KERNEL_TYPED(double)
 
 }  // namespace Lotus
--- lotus/core/providers/cuda/tensor/reshape.cc
+++ lotus/core/providers/cuda/tensor/reshape.cc
@@ -2,22 +2,29 @@
 
 namespace Lotus {
 namespace Cuda {
-REGISTER_KERNEL(KernelDefBuilder("Reshape")
-                    .Domain(LotusIR::kOnnxDomain)
-                    .SinceVersion(5)
-                    .Provider(LotusIR::kCudaExecutionProvider)
-                    .Alias(0, 0)
-                    .InputMemoryType<kMemTypeCPUInput>(1)
-                    .TypeConstraint("T", DataTypeImpl::GetTensorType<float>())
-                    .TypeConstraint("shape", DataTypeImpl::GetTensorType<int64_t>()),
-                Reshape<float>);
 
-REGISTER_KERNEL(KernelDefBuilder("Reshape")
-                    .Domain(LotusIR::kOnnxDomain)
-                    .SinceVersion(1, 4)
-                    .Provider(LotusIR::kCudaExecutionProvider)
-                    .Alias(0, 0)
-                    .TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
-                Reshape_1<float>);
+#define REGISTER_KERNEL_TYPED(T)                                                        \
+  REGISTER_KERNEL(KernelDefBuilder("Reshape")                                           \
+                      .Domain(LotusIR::kOnnxDomain)                                     \
+                      .SinceVersion(5)                                                  \
+                      .Provider(LotusIR::kCudaExecutionProvider)                        \
+                      .Alias(0, 0)                                                      \
+                      .InputMemoryType<kMemTypeCPUInput>(1)                             \
+                      .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())            \
+                      .TypeConstraint("shape", DataTypeImpl::GetTensorType<int64_t>()), \
+                  Reshape<T>);                                                          \
+                                                                                        \
+  REGISTER_KERNEL(KernelDefBuilder("Reshape")                                           \
+                      .Domain(LotusIR::kOnnxDomain)                                     \
+                      .SinceVersion(1, 4)                                               \
+                      .Provider(LotusIR::kCudaExecutionProvider)                        \
+                      .Alias(0, 0)                                                      \
+                      .TypeConstraint("T", DataTypeImpl::GetTensorType<T>()),           \
+                  Reshape_1<T>);
+
+REGISTER_KERNEL_TYPED(MLFloat16)
+REGISTER_KERNEL_TYPED(float)
+REGISTER_KERNEL_TYPED(double)
+
 }  // namespace Cuda
 }  // namespace Lotus
