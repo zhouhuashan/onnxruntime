ef9ec80c3f36f3f0d4c20010e81cc0d732ef286f
Hector Li <hecli@microsoft.com>
2018-07-11 18:55:33 +0000

    Merged PR 2030: set the public eigen github repo as default in eigen cmake
    set the public eigen github repo as default in eigen cmake

--- cmake/external/eigen.cmake
+++ cmake/external/eigen.cmake
@@ -1,16 +1,9 @@
 include (ExternalProject)
 
-set(eigen_URL "https://ossmsft.visualstudio.com/DefaultCollection/Project15/_git/Archive_Eigen")
-set(eigen_TAG "Microsoft/3.3.4")
-#set(eigen_BUILD ${CMAKE_CURRENT_BINARY_DIR}/eigen/src/eigen)
-#set(eigen_INSTALL ${CMAKE_CURRENT_BINARY_DIR}/eigen/install)
-
 if (lotus_USE_PREINSTALLED_EIGEN)
     set(eigen_INCLUDE_DIRS ${eigen_SOURCE_PATH})
     ExternalProject_Add(eigen
         PREFIX eigen
-        GIT_REPOSITORY ${eigen_URL}
-        GIT_TAG ${eigen_TAG}
         SOURCE_DIR ${eigen_SOURCE_PATH}
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
@@ -19,16 +12,17 @@ if (lotus_USE_PREINSTALLED_EIGEN)
         UPDATE_COMMAND ""
     )
 else ()
-    set(eigen_INCLUDE_DIRS
-        ${CMAKE_CURRENT_BINARY_DIR}/eigen/src/eigen/eigen-eigen-5a0156e40feb
-    )
+    set(eigen_URL "https://github.com/eigenteam/eigen-git-mirror.git")
+    set(eigen_TAG "3.3.4")
+    set(eigen_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/external/eigen)
+    set(eigen_INCLUDE_DIRS ${eigen_ROOT_DIR})
     ExternalProject_Add(eigen
         PREFIX eigen
         GIT_REPOSITORY ${eigen_URL}
         GIT_TAG ${eigen_TAG}
+        SOURCE_DIR ${eigen_ROOT_DIR}
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
-        #DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
     )
 endif()
