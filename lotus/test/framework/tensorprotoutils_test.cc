626718662e314831164144c43d24d5ad4db5a014
Changming Sun <chasun@microsoft.com>
2018-07-18 07:28:02 +0000

    Merged PR 2079: Add a test case for GetTensorFromTensorProto function
    Add a test case for GetTensorFromTensorProto function

---
+++ lotus/test/framework/tensorprotoutils_test.cc
@@ -0,0 +1,26 @@
+#include "core/platform/env.h"
+#include "core/framework/tensor.h"
+#include "core/framework/tensorprotoutils.h"
+
+#include "gtest/gtest.h"
+
+#include <sstream>
+
+namespace Lotus {
+namespace Test {
+#ifdef LOTUSIR_RUN_EXTERNAL_ONNX_TESTS
+TEST(TensorProtoUtilsTest, test1) {
+  const char* filename = "../models/test_resnet50/test_data_set_0/input_0.pb";
+  std::string tensorbinary;
+  Common::Status st = Env::Default().ReadFileAsString(filename, &tensorbinary);
+  ASSERT_TRUE(st.IsOK());
+  onnx::TensorProto proto;
+  ASSERT_TRUE(proto.ParseFromString(tensorbinary));
+  std::unique_ptr<Tensor> tensor;
+  Lotus::AllocatorPtr cpu_allocator = std::make_shared<Lotus::CPUAllocator>();
+  st = Lotus::Utils::GetTensorFromTensorProto(proto, &tensor, cpu_allocator);
+  ASSERT_TRUE(st.IsOK());
+}
+#endif
+}  // namespace Test
+}  // namespace Lotus
