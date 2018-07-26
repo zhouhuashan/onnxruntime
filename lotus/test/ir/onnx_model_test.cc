zhouhuashan/onnxruntime

commit f478dac9876b4f8e0a2935eee2c1e10a7c8edc67

Merged PR 2092: Move FileOpenRd/FileOpenWr/FileClose to Env class
Move FileOpenRd/FileOpenWr/FileClose to Env class
Delete lotus/core/graph/utils.h
Add a new method "GetSelfPid" to Env class. Mainly for setting random seed.
Add "-Wno-error=invalid-partial-specialization" to clang cflags, because we reverted eigen to an older version and there is a warning in eigen that we can't fix.
Remove 'virtual' keyword from ILotusOpSchemaCollection::GetSchema because this function is already declared 'final'
Add a new macro "LOTUS_MUST_USE_RESULT", and apply it to GetAttr() functions.

Related work items: #481

lotus/core/graph/graph.cc
@@ -11,7 +11,6 @@
 #include "gsl/pointers"
 #include "core/graph/graph.h"
 #include "core/graph/op.h"
-#include "core/graph/utils.h"
 #include "core/common/logging/logging.h"
 #include "onnx/checker.h"
 #include "core/graph/schema_registry.h"

lotus/core/graph/graph.h
@@ -16,7 +16,6 @@
 #include "core/graph/constants.h"
 #include "core/graph/graph_nodes.h"
 #include "onnx/defs/schema.h"
-#include "core/graph/utils.h"
 #include "onnx/onnx_pb.h"
 
 // TODO - Evaluate switching the types below to support transparent comparators and enable

lotus/core/graph/model.cc
@@ -1,3 +1,7 @@
+#include "core/graph/model.h"
+
+#include <memory>
+
 #ifdef _MSC_VER
 #pragma warning(push)
 // 'type' : forcing value to bool 'true' or 'false' (performance warning)
@@ -9,12 +13,11 @@
 #endif
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
-#include <memory>
-#include "core/graph/model.h"
-#include "core/graph/utils.h"
-#include "core/graph/schema_registry.h"
 #include "gsl/pointers"
 #include "gsl/gsl_util"
+
+#include "core/platform/env.h"
+#include "core/graph/schema_registry.h"
 using namespace onnx;
 using namespace Lotus;
 using namespace Lotus::Common;
@@ -31,7 +34,7 @@ Model::Model(const std::string& graph_name, bool is_onnx_domain_only, const Mode
     prop->set_value(metadata.second);
   }
 
-  auto schema_registry = std::shared_ptr<SchemaRegistryManager>(new SchemaRegistryManager());
+  auto schema_registry = std::make_shared<SchemaRegistryManager>();
   if (local_registries != nullptr) {
     for (auto schema_collection : *local_registries) {
       schema_registry->RegisterRegistry(schema_collection);
@@ -61,7 +64,7 @@ Model::Model(std::unique_ptr<ModelProto> model_proto, const ILotusOpSchemaRegist
     model_metadata_[prop.key()] = prop.value();
   }
 
-  auto schema_registry = std::shared_ptr<SchemaRegistryManager>(new SchemaRegistryManager());
+  auto schema_registry = std::make_shared<SchemaRegistryManager>();
   if (local_registries != nullptr) {
     for (auto schema_collection : *local_registries) {
       schema_registry->RegisterRegistry(schema_collection);
@@ -209,17 +212,17 @@ GSL_SUPPRESS(r .30)  // spurious warnings. p_model is potentially reset in the i
 GSL_SUPPRESS(r .35)
 Status Model::Load(const std::wstring& file_path, std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
   int fd;
-  LOTUS_RETURN_IF_ERROR(FileOpenRd(file_path, &fd));
+  LOTUS_RETURN_IF_ERROR(Env::Default().FileOpenRd(file_path, &fd));
   auto status = Load(fd, p_model, local_registries);
-  LOTUS_RETURN_IF_ERROR(FileClose(fd));
+  LOTUS_RETURN_IF_ERROR(Env::Default().FileClose(fd));
   return status;
 }
 
 Status Model::Save(Model& model, const std::wstring& file_path) {
   int fd;
-  LOTUS_RETURN_IF_ERROR(FileOpenWr(file_path, &fd));
+  LOTUS_RETURN_IF_ERROR(Env::Default().FileOpenWr(file_path, &fd));
   auto status = Save(model, fd);
-  LOTUS_RETURN_IF_ERROR(FileClose(fd));
+  LOTUS_RETURN_IF_ERROR(Env::Default().FileClose(fd));
   return status;
 }
 
@@ -229,12 +232,17 @@ GSL_SUPPRESS(r .30)  // spurious warnings. p_model is potentially reset in the i
 GSL_SUPPRESS(r .35)
 Status Model::Load(const std::string& file_path, std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
   int fd;
-  if (!FileOpenRd(file_path, &fd).IsOK()) {
+  Status status = Env::Default().FileOpenRd(file_path, &fd);
+  if (!status.IsOK()) {
     return Status(LOTUS, NO_MODEL, "Failed to open: " + file_path);
   }
-  auto status = Load(fd, p_model, local_registries);
-  LOTUS_RETURN_IF_ERROR(FileClose(fd));
-  return status;
+  try {
+    status = Load(fd, p_model, local_registries);
+  } catch (std::exception& ex) {
+    Env::Default().FileClose(fd);
+    return LOTUS_MAKE_STATUS(LOTUS, FAIL, "Failed to load: " + file_path, ". ", ex.what());
+  }
+  return Env::Default().FileClose(fd);
 }
 
 Status Model::LoadFromBytes(int count, void* p_bytes, /*out*/ std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
@@ -253,10 +261,14 @@ Status Model::LoadFromBytes(int count, void* p_bytes, /*out*/ std::shared_ptr<Mo
 
 Status Model::Save(Model& model, const std::string& file_path) {
   int fd;
-  LOTUS_RETURN_IF_ERROR(FileOpenWr(file_path, &fd));
-  auto status = Save(model, fd);
-  LOTUS_RETURN_IF_ERROR(FileClose(fd));
-  return status;
+  LOTUS_RETURN_IF_ERROR(Env::Default().FileOpenWr(file_path, &fd));
+  try {
+    auto status = Save(model, fd);
+  } catch (std::exception& ex) {
+    Env::Default().FileClose(fd);
+    return LOTUS_MAKE_STATUS(LOTUS, FAIL, "Failed to save model to " + file_path, ". ", ex.what());
+  }
+  return Env::Default().FileClose(fd);
 }
 
 using ::google::protobuf::io::CodedInputStream;

lotus/core/graph/schema_registry.cc
@@ -37,8 +37,8 @@ Lotus::Common::Status LotusOpSchemaRegistry::RegisterOpSet(
     int baseline_opset_version,
     int opset_version) {
   LOTUS_RETURN_IF_ERROR(SetBaselineAndOpsetVersionForDomain(domain, baseline_opset_version, opset_version));
-  for (int i = 0; i < schemas.size(); i++)
-    LOTUS_RETURN_IF_ERROR(RegisterOpSchema(std::move(schemas[i])));
+  for (auto& schema : schemas)
+    LOTUS_RETURN_IF_ERROR(RegisterOpSchema(std::move(schema)));
   return Lotus::Common::Status::OK();
 }
 

lotus/test/ir/onnx_model_test.cc
@@ -1,5 +1,6 @@
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <memory>
+#include "core/platform/env.h"
 #include "core/graph/graph.h"
 #include "core/graph/model.h"
 #include "core/graph/op.h"
@@ -63,7 +64,7 @@ TEST(ONNXModelsTest, bvlc_alexnet) {
   using ::google::protobuf::io::FileInputStream;
   using ::google::protobuf::io::ZeroCopyInputStream;
   int fd;
-  FileOpenRd("../models/test_bvlc_alexnet/model.onnx", &fd);
+  ASSERT_TRUE(Env::Default().FileOpenRd("../models/test_bvlc_alexnet/model.onnx", &fd).IsOK());
   ASSERT_TRUE(fd > 0);
   std::unique_ptr<ZeroCopyInputStream> raw_input(new FileInputStream(fd));
   std::unique_ptr<CodedInputStream> coded_input(new CodedInputStream(raw_input.get()));
@@ -74,7 +75,7 @@ TEST(ONNXModelsTest, bvlc_alexnet) {
   coded_input.reset();
   raw_input.reset();
   EXPECT_TRUE(result);
-  FileClose(fd);
+  ASSERT_TRUE(Env::Default().FileClose(fd).IsOK());
 
   std::shared_ptr<Model> model;
   ASSERT_TRUE(Model::Load("../models/test_bvlc_alexnet/model.onnx", model).IsOK());

commit 6277ac80ee5a5c714bf4cec34290ae7618e0bd94

Merged PR 2181: Fix a bug in Model::Load()
Fix a bug in Model::Load(). The bug was introduced in commit f478dac9876b4f8e0a2935eee2c1e10a7c8edc67.

Suppress some warnings:
https://developercommunity.visualstudio.com/content/problem/215975/c-code-analysis-c26444-false-positive.html

lotus/core/graph/graph.cc
@@ -202,7 +202,7 @@ void Node::Init(const std::string& name,
   definitions_.output_defs = output_args;
   domain_ = domain;
   if (kOnnxDomainAlias == domain_) {
-	  domain_ = kOnnxDomain;
+    domain_ = kOnnxDomain;
   }
 
   // Set each arg count as 1 by default.
@@ -1040,6 +1040,7 @@ Status Graph::VerifyNodeAndOpMatch(const std::vector<NodeIndex>& nodes_in_topolo
     ctx.set_schema_registry(schema_registry_.get());
     LexicalScopeContext lsc;
     for (auto& kv : output_args) {
+      GSL_SUPPRESS(es .84)
       lsc.output_names.insert(kv.first);
     }
     NodeProto node_proto;
@@ -1105,6 +1106,7 @@ Status Graph::VerifyInputAndInitializerNames(/*OUT*/ std::unordered_set<std::str
     }
   }
   for (auto& initializer_pair : name_to_initial_tensor_) {
+    GSL_SUPPRESS(es .84)
     inputs_and_initializers.insert(initializer_pair.first);
     // Initializers are expected to be included in inputs (according to ONNX spec).
     // Lotus relaxes this constraint. No duplicate-name check here.
@@ -1496,8 +1498,10 @@ void Graph::SyncGraphInputsOutputs() {
 void Graph::CleanUnusedInitializers() {
   std::vector<std::string> unused_names;
   std::set<const NodeArg*> input_args;
-  for (const auto& node : Nodes())
-    node.ForEachInputDef([&input_args](const LotusIR::NodeArg* def) { input_args.insert(def); });
+  for (const auto& node : Nodes()) {
+    node.ForEachInputDef([&input_args](const LotusIR::NodeArg* def) { GSL_SUPPRESS(es .84)
+                                                                      input_args.insert(def); });
+  }
 
   for (const auto& pv : name_to_initial_tensor_) {
     const std::string& s = pv.first;

lotus/core/graph/graph.h
@@ -444,6 +444,7 @@ class GraphBase {
 
     owned_node_args_.push_back(std::make_unique<NodeArg>(name, p_arg_type));
     NodeArg* new_arg = owned_node_args_.back().get();
+    GSL_SUPPRESS(es .84)
     node_args_.insert(std::make_pair(name, new_arg));
     return *new_arg;
   }

lotus/core/graph/model.cc
@@ -216,23 +216,66 @@ Status Model::Load(std::unique_ptr<ModelProto> p_model_proto, std::shared_ptr<Mo
   return Status::OK();
 }
 
+template <typename T>
+static Status LoadModel(const T& file_path, std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
+  int fd;
+  Status status = Env::Default().FileOpenRd(file_path, &fd);
+  if (!status.IsOK()) {
+    if (status.Category() == Common::SYSTEM) {
+      switch (status.Code()) {
+        case ENOENT:
+          return LOTUS_MAKE_STATUS(LOTUS, NO_SUCHFILE, "Load model failed. File doesn't exist");
+        case EINVAL:
+          return LOTUS_MAKE_STATUS(LOTUS, INVALID_ARGUMENT);
+        default:
+          return LOTUS_MAKE_STATUS(LOTUS, FAIL, "system error number ", status.Code());
+      }
+    }
+  }
+  try {
+    status = Model::Load(fd, p_model, local_registries);
+  } catch (std::exception& ex) {
+    GSL_SUPPRESS(es .84)
+    (void) Env::Default().FileClose(fd);
+    return Status(LOTUS, FAIL, ex.what());
+  }
+  if (!status.IsOK()) {
+    GSL_SUPPRESS(es .84)
+    (void) Env::Default().FileClose(fd);
+    return status;
+  }
+  return Env::Default().FileClose(fd);
+}
+
+template <typename T>
+static Status SaveModel(Model& model, const T& file_path) {
+  int fd;
+  Status status = Env::Default().FileOpenWr(file_path, &fd);
+  LOTUS_RETURN_IF_ERROR(status);
+  try {
+    status = Model::Save(model, fd);
+  } catch (std::exception& ex) {
+    GSL_SUPPRESS(es .84)
+    (void) Env::Default().FileClose(fd);
+    return Status(LOTUS, FAIL, ex.what());
+  }
+  if (!status.IsOK()) {
+    GSL_SUPPRESS(es .84)
+    (void) Env::Default().FileClose(fd);
+    return status;
+  }
+  return Env::Default().FileClose(fd);
+}
+
 #ifdef _WIN32
 GSL_SUPPRESS(r .30)  // spurious warnings. p_model is potentially reset in the internal call to Load
 GSL_SUPPRESS(r .35)
 Status Model::Load(const std::wstring& file_path, std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
-  int fd;
-  LOTUS_RETURN_IF_ERROR(Env::Default().FileOpenRd(file_path, &fd));
-  auto status = Load(fd, p_model, local_registries);
-  LOTUS_RETURN_IF_ERROR(Env::Default().FileClose(fd));
-  return status;
+  return LoadModel(file_path, p_model, local_registries);
 }
 
 Status Model::Save(Model& model, const std::wstring& file_path) {
-  int fd;
-  LOTUS_RETURN_IF_ERROR(Env::Default().FileOpenWr(file_path, &fd));
-  auto status = Save(model, fd);
-  LOTUS_RETURN_IF_ERROR(Env::Default().FileClose(fd));
-  return status;
+  return SaveModel(model, file_path);
 }
 
 #endif
@@ -240,18 +283,11 @@ Status Model::Save(Model& model, const std::wstring& file_path) {
 GSL_SUPPRESS(r .30)  // spurious warnings. p_model is potentially reset in the internal call to Load
 GSL_SUPPRESS(r .35)
 Status Model::Load(const std::string& file_path, std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
-  int fd;
-  Status status = Env::Default().FileOpenRd(file_path, &fd);
-  if (!status.IsOK()) {
-    return Status(LOTUS, NO_MODEL, "Failed to open: " + file_path);
-  }
-  try {
-    status = Load(fd, p_model, local_registries);
-  } catch (std::exception& ex) {
-    Env::Default().FileClose(fd);
-    return LOTUS_MAKE_STATUS(LOTUS, FAIL, "Failed to load: " + file_path, ". ", ex.what());
-  }
-  return Env::Default().FileClose(fd);
+  return LoadModel(file_path, p_model, local_registries);
+}
+
+Status Model::Save(Model& model, const std::string& file_path) {
+  return SaveModel(model, file_path);
 }
 
 Status Model::LoadFromBytes(int count, void* p_bytes, /*out*/ std::shared_ptr<Model>& p_model, const ILotusOpSchemaRegistryList* local_registries) {
@@ -266,18 +302,6 @@ Status Model::LoadFromBytes(int count, void* p_bytes, /*out*/ std::shared_ptr<Mo
     LOTUS_RETURN_IF_ERROR(p_model->MainGraph()->Resolve(true));
   }
   return Status::OK();
-}
-
-Status Model::Save(Model& model, const std::string& file_path) {
-  int fd;
-  LOTUS_RETURN_IF_ERROR(Env::Default().FileOpenWr(file_path, &fd));
-  try {
-    auto status = Save(model, fd);
-  } catch (std::exception& ex) {
-    Env::Default().FileClose(fd);
-    return LOTUS_MAKE_STATUS(LOTUS, FAIL, "Failed to save model to " + file_path, ". ", ex.what());
-  }
-  return Env::Default().FileClose(fd);
 }
 
 using ::google::protobuf::io::CodedInputStream;

lotus/core/graph/schema_registry.cc
@@ -89,7 +89,7 @@ Lotus::Common::Status LotusOpSchemaRegistry::RegisterOpSchemaInternal(ONNX_NAMES
         << "than the operator set version " << ver_range_it->second.opset_version << std::endl;
     return Lotus::Common::Status(Lotus::Common::LOTUS, Lotus::Common::INVALID_ARGUMENT, ostream.str());
   }
-
+  GSL_SUPPRESS(es .84)
   map_[op_name][op_domain].emplace(std::make_pair(ver, op_schema));
   return Lotus::Common::Status::OK();
 }
@@ -164,6 +164,7 @@ Domain_To_Version_Map SchemaRegistryManager::GetLatestOpsetVersions(bool is_onnx
       // If the map doesn't yet contain this domain, insert it with this registry's value.
       // Otherwise, merge the existing range in the map.
       if (iter == domain_version_map.end()) {
+        GSL_SUPPRESS(es .84)
         domain_version_map.insert(local_domain);
       } else {
         iter->second = std::max(iter->second, local_domain.second);
@@ -180,7 +181,8 @@ Domain_To_Version_Map SchemaRegistryManager::GetLatestOpsetVersions(bool is_onnx
       continue;
     auto it = domain_version_map.find(domain.first);
     if (it == domain_version_map.end()) {
-      domain_version_map.insert(std::pair<std::string, int>(domain.first, domain.second.second));
+      GSL_SUPPRESS(es .84)
+      domain_version_map.insert(std::make_pair(domain.first, domain.second.second));
     } else {
       it->second = std::max(it->second, domain.second.second);
     }
@@ -222,6 +224,7 @@ void SchemaRegistryManager::GetSchemaAndHistory(
     }
 
     if (new_version < version) {
+      GSL_SUPPRESS(es .84)
       unchecked_registry_indices.insert(unchecked_registry_indices.end(),
                                         checked_registry_indices.begin(),
                                         checked_registry_indices.end());

lotus/test/ir/onnx_model_test.cc
@@ -14,7 +14,7 @@ namespace Test {
 // Tests that Resolve() properly clears the state of topological sorted nodes,
 // inputs, outputs and valueInfo.
 // Assumes the graph passed in has been previously resolved.
-void TestResolve(LotusIR::Graph* p_graph) {
+static void TestResolve(LotusIR::Graph* p_graph) {
   const std::vector<LotusIR::NodeIndex>* nodes;
   EXPECT_TRUE(p_graph->GetNodesInTopologicalOrder(&nodes).IsOK());
   auto nodes_before = *nodes;
@@ -31,7 +31,7 @@ void TestResolve(LotusIR::Graph* p_graph) {
   EXPECT_TRUE(p_graph->Resolve().IsOK());
 
   const std::vector<LotusIR::NodeIndex>* nodes_after;
-  p_graph->GetNodesInTopologicalOrder(&nodes_after);
+  EXPECT_TRUE(p_graph->GetNodesInTopologicalOrder(&nodes_after).IsOK());
   auto& inputs_after = p_graph->GetInputs();
   auto& outputs_after = p_graph->GetOutputs();
   auto& value_info_after = p_graph->GetValueInfo();
@@ -58,8 +58,22 @@ TEST(ONNXModelsTest, squeeze_net) {
 #endif
 }
 
+TEST(ONNXModelsTest, non_existing_model) {
+  // NOTE: this requires the current directory to be where LotusIR_UT.exe is located
+  std::shared_ptr<Model> model;
+  Common::Status st = Model::Load("./testdata/non_existing_model_XXXXXX/model.onnx", model);
+  ASSERT_FALSE(st.IsOK());
+  ASSERT_EQ(st.Code(), Common::NO_SUCHFILE);
+#ifdef _WIN32
+  // wstring version
+  std::shared_ptr<Model> model2;
+  ASSERT_FALSE(Model::Load(L"./testdata/non_existing_model_XXXXXX/model.onnx", model2).IsOK());
+  ASSERT_EQ(st.Code(), Common::NO_SUCHFILE);
+#endif
+}
+
 #ifdef LOTUSIR_RUN_EXTERNAL_ONNX_TESTS
-TEST(ONNXModelsTest, bvlc_alexnet) {
+TEST(ONNXModelsTest1, bvlc_alexnet_1) {
   using ::google::protobuf::io::CodedInputStream;
   using ::google::protobuf::io::FileInputStream;
   using ::google::protobuf::io::ZeroCopyInputStream;
@@ -87,53 +101,50 @@ TEST(ONNXModelsTest, bvlc_alexnet) {
   TestResolve(model->MainGraph());
 }
 
-TEST(ONNXModelsTest, densenet121) {
+class ONNXModelsTest : public ::testing::TestWithParam<const char*> {
+  // You can implement all the usual fixture class members here.
+  // To access the test parameter, call GetParam() from class
+  // TestWithParam<T>.
+ public:
+  std::string GetModelFileName() const {
+    std::ostringstream oss;
+    oss << "../models/test_" << GetParam() << "/model.onnx";
+    return oss.str();
+  }
+};
+
+TEST_P(ONNXModelsTest, LoadFromFile) {
   std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_densenet121/model.onnx", model).IsOK());
+  ASSERT_TRUE(Model::Load(GetModelFileName(), model).IsOK());
   TestResolve(model->MainGraph());
 }
 
-TEST(ONNXModelsTest, inception_v1) {
+TEST_P(ONNXModelsTest, LoadFromProtobuf) {
+  using ::google::protobuf::io::CodedInputStream;
+  using ::google::protobuf::io::FileInputStream;
+  using ::google::protobuf::io::ZeroCopyInputStream;
+  int fd;
+  auto st = Env::Default().FileOpenRd(GetModelFileName(), &fd);
+  ASSERT_TRUE(st.IsOK()) << st.ErrorMessage();
+  ASSERT_TRUE(fd > 0);
+  std::unique_ptr<ZeroCopyInputStream> raw_input(new FileInputStream(fd));
+  std::unique_ptr<CodedInputStream> coded_input(new CodedInputStream(raw_input.get()));
+  coded_input->SetTotalBytesLimit(INT_MAX, INT_MAX);
+  std::unique_ptr<ModelProto> model_proto = std::make_unique<ModelProto>();
+  bool result = model_proto->ParseFromCodedStream(coded_input.get());
+  coded_input.reset();
+  raw_input.reset();
+  ASSERT_TRUE(result);
+  ASSERT_TRUE(Env::Default().FileClose(fd).IsOK());
   std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_inception_v1/model.onnx", model).IsOK());
+  ASSERT_TRUE(Model::Load(std::move(model_proto), model).IsOK());
   TestResolve(model->MainGraph());
 }
 
-TEST(ONNXModelsTest, inception_v2) {
-  std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_inception_v2/model.onnx", model).IsOK());
-  TestResolve(model->MainGraph());
-}
+INSTANTIATE_TEST_CASE_P(ONNXModelsTests,
+                        ONNXModelsTest,
+                        ::testing::Values("bvlc_alexnet", "bvlc_googlenet", "bvlc_reference_caffenet", "bvlc_reference_rcnn_ilsvrc13", "densenet121", "emotion_ferplus", "inception_v1", "inception_v2", "mnist", "resnet50", "shufflenet", "squeezenet", "tiny_yolov2", "vgg19", "zfnet"));
 
-TEST(ONNXModelsTest, resnet50) {
-  std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_resnet50/model.onnx", model).IsOK());
-  TestResolve(model->MainGraph());
-}
-
-TEST(ONNXModelsTest, shufflenet) {
-  std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_shufflenet/model.onnx", model).IsOK());
-  TestResolve(model->MainGraph());
-}
-
-TEST(ONNXModelsTest, squeezenet) {
-  std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_squeezenet/model.onnx", model).IsOK());
-  TestResolve(model->MainGraph());
-}
-
-TEST(ONNXModelsTest, zfnet) {
-  std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_zfnet/model.onnx", model).IsOK());
-  TestResolve(model->MainGraph());
-}
-
-TEST(ONNXModelsTest, vgg19) {
-  std::shared_ptr<Model> model;
-  ASSERT_TRUE(Model::Load("../models/test_vgg19/model.onnx", model).IsOK());
-  TestResolve(model->MainGraph());
-}
 #endif
 }  // namespace Test
 }  // namespace LotusIR