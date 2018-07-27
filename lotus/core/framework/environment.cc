3dfe804bb57bb623af644ac2c06445793a942859
Cheng Tang <chenta@microsoft.com>
2018-07-27 17:38:28 +0000

    Merged PR 2191: remove duplicate registry caused by merge
    remove duplicate registry caused by merge

--- lotus/core/framework/environment.cc
+++ lotus/core/framework/environment.cc
@@ -51,33 +51,6 @@ Internal copy node
     status = LotusIR::MsOpRegistry::RegisterMsOps();
     LOTUS_RETURN_IF_ERROR(status);
 
-    // Register MemCpy schema;
-
-    // These ops are internal-only, so register outside of onnx
-    ONNX_OPERATOR_SCHEMA(MemcpyFromHost)
-        .Input(0, "X", "input", "T")
-        .Output(0, "Y", "output", "T")
-        .TypeConstraint(
-            "T",
-            OpSchema::all_tensor_types(),
-            "Constrain to any tensor type. If the dtype attribute is not provided this must be a valid output type.")
-        .TypeAndShapeInferenceFunction(propagateShapeAndTypeFromFirstInput)
-        .SetDoc(R"DOC(
-                Internal copy node
-                )DOC");
-
-    ONNX_OPERATOR_SCHEMA(MemcpyToHost)
-        .Input(0, "X", "input", "T")
-        .Output(0, "Y", "output", "T")
-        .TypeConstraint(
-            "T",
-            OpSchema::all_tensor_types(),
-            "Constrain to any tensor type. If the dtype attribute is not provided this must be a valid output type.")
-        .TypeAndShapeInferenceFunction(propagateShapeAndTypeFromFirstInput)
-        .SetDoc(R"DOC(
-                Internal copy node
-                )DOC");
-
     // TODO: Should register microsoft domain kernels here.
 
   } catch (std::exception& ex) {
