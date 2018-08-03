9f0f74af8f8965b8a8fc45075b0926a35ed4a722
Ryan Hill <ryanhill@microsoft.com>
2018-08-03 20:53:34 +0000

    Merged PR 2255: In cast_op.h, pull shape.Size() calculation out of the loop
    It's an optimization suggested by the windows team based on benchmarks they ran
    
    Related work items: #1066

--- lotus/core/providers/cpu/tensor/cast_op.h
+++ lotus/core/providers/cpu/tensor/cast_op.h
@@ -13,7 +13,8 @@ template <typename SrcType,
 inline void CastData(const Tensor* in, Tensor* out, const TensorShape& shape) {
   auto out_data = out->MutableData<DstType>();
   auto in_data = in->Data<SrcType>();
-  for (int64_t i = 0; i < shape.Size(); ++i) {
+  auto shape_size = shape.Size();
+  for (int64_t i = 0; i < shape_size; ++i) {
     out_data[i] = static_cast<DstType>(in_data[i]);
   }
 }
@@ -22,7 +23,8 @@ template <>
 inline void CastData<float, MLFloat16>(const Tensor* in, Tensor* out, const TensorShape& shape) {
   auto out_data = out->MutableData<MLFloat16>();
   auto in_data = in->Data<float>();
-  for (int64_t i = 0; i < shape.Size(); ++i) {
+  auto shape_size = shape.Size();
+  for (int64_t i = 0; i < shape_size; ++i) {
     out_data[i] = MLFloat16(Eigen::half_impl::float_to_half_rtne(in_data[i]).x);
   }
 }
@@ -31,7 +33,8 @@ template <>
 inline void CastData<MLFloat16, float>(const Tensor* in, Tensor* out, const TensorShape& shape) {
   auto out_data = out->MutableData<float>();
   auto in_data = in->Data<MLFloat16>();
-  for (int64_t i = 0; i < shape.Size(); ++i) {
+  auto shape_size = shape.Size();
+  for (int64_t i = 0; i < shape_size; ++i) {
     out_data[i] = Eigen::half_impl::half_to_float(Eigen::half_impl::__half(in_data[i].val));
   }
 }
