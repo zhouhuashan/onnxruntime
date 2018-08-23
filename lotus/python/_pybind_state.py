c36fd9622a98bec399dd10ed3bc57f71337c1658
xavier dupré <xavier.dupre@gmail.com>
2018-08-23 13:55:21 +0200

    applies autopep8 on python source

--- lotus/python/_pybind_state.py
+++ lotus/python/_pybind_state.py
@@ -1,4 +1,4 @@
-## @package _pybind_state
+# @package _pybind_state
 # Module lotus.python._pybind_state
 import sys
 import os
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 try:
     from lotus.python.lotus_pybind11_state import *  # noqa
-    
 except ImportError as e:
     logging.critical(
         'Cannot load lotus.python. Error: {0}'.format(str(e)))
--- lotus/python/session.py
+++ lotus/python/session.py
@@ -1,4 +1,4 @@
-## @package session
+# @package session
 # Module lotus.python.session
 # TODO: remove this import as we don't support Python 2
 # TODO: switch to 4 characters for indentation, most editors are set up this way.
@@ -15,63 +15,67 @@ import os
 logger = logging.getLogger(__name__)
 from lotus.python import _pybind_state as C
 
+
 class InferenceSession:
-  """
-  This is the main class used to run a model.
-  See example :ref:`l-example-simple-usage`
-  for a simple sample of usage.
-  """
-  def __init__(self, path, sess_options=None):
-    if sess_options:
-      self._sess = C.InferenceSession(
-        sess_options, C.get_session_initializer())
-    else:
-      self._sess = C.InferenceSession(
-        C.get_session_initializer(), C.get_session_initializer())
-    self._sess.load_model(path)
-    self._inputs_meta = self._sess.inputs_meta
-    self._outputs_meta = self._sess.outputs_meta
-    self._model_meta = self._sess.model_meta
-
-  def get_inputs(self):
-    "Return the inputs metadata."
-    return self._inputs_meta
-
-  def get_outputs(self):
-    "Return the outputs metadata."
-    return self._outputs_meta
-
-  def get_modelmeta(self):
-    "Return the metadata."
-    return self._model_meta
-
-  def run(self, output_names, input_feed, run_options=None):
-    """Compute the predictions.
-    :param output_names: name of the outputs
-    :param input_feed: dictionary ``{ input_name: input_value }``
-    :param run_options: See :class:`lotus.RunOptions`.
-    
-    ::
-    
-        sess.run([output_name], {input_name: x})
-
-    See :ref:`l-example-simple-usage`
-    for a complete example of usage.
     """
-    num_required_inputs = len(self._inputs_meta)
-    num_inputs = len(input_feed)
-    if num_inputs != num_required_inputs:
-        raise ValueError("Model requires {} inputs. Input Feed contains {}".format(num_required_inputs,
-                                                                                   num_inputs))
-    if not output_names:
-      output_names = [ output.name for output in self._outputs_meta ]
-    return self._sess.run(output_names, input_feed, run_options)
+    This is the main class used to run a model.
+    See example :ref:`l-example-simple-usage`
+    for a simple sample of usage.
+    """
 
-  def end_profiling(self):
-    "End profiling and return results."
-    return self._sess.end_profiling()
+    def __init__(self, path, sess_options=None):
+        if sess_options:
+            self._sess = C.InferenceSession(
+                sess_options, C.get_session_initializer())
+        else:
+            self._sess = C.InferenceSession(
+                C.get_session_initializer(), C.get_session_initializer())
+        self._sess.load_model(path)
+        self._inputs_meta = self._sess.inputs_meta
+        self._outputs_meta = self._sess.outputs_meta
+        self._model_meta = self._sess.model_meta
+
+    def get_inputs(self):
+        "Return the inputs metadata."
+        return self._inputs_meta
+
+    def get_outputs(self):
+        "Return the outputs metadata."
+        return self._outputs_meta
+
+    def get_modelmeta(self):
+        "Return the metadata."
+        return self._model_meta
+
+    def run(self, output_names, input_feed, run_options=None):
+        """Compute the predictions.
+        :param output_names: name of the outputs
+        :param input_feed: dictionary ``{ input_name: input_value }``
+        :param run_options: See :class:`lotus.RunOptions`.
+
+        ::
+
+            sess.run([output_name], {input_name: x})
+
+        See :ref:`l-example-simple-usage`
+        for a complete example of usage.
+        """
+        num_required_inputs = len(self._inputs_meta)
+        num_inputs = len(input_feed)
+        if num_inputs != num_required_inputs:
+            raise ValueError("Model requires {} inputs. Input Feed contains {}".format(num_required_inputs,
+                                                                                       num_inputs))
+        if not output_names:
+            output_names = [output.name for output in self._outputs_meta]
+        return self._sess.run(output_names, input_feed, run_options)
+
+    def end_profiling(self):
+        "End profiling and return results."
+        return self._sess.end_profiling()
+
 
 if __name__ == "__main__":
-  # TODO: remove this piece of code.
-  if not 'InferenceSession' in dir(C):
-    raise RuntimeError('Failed to bind the native module -lotus_pybind11_state.pyd.')
+    # TODO: remove this piece of code.
+    if not 'InferenceSession' in dir(C):
+        raise RuntimeError(
+            'Failed to bind the native module -lotus_pybind11_state.pyd.')
