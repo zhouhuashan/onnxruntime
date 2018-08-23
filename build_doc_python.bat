7eeb9ca50eb1826323dc39a12c6fafe76b4cf31e
xavier dupré <xavier.dupre@gmail.com>
2018-08-23 19:15:38 +0200

    remove --pybind11 as it is triggered by --build_wheel

--- build_doc_python.bat
+++ build_doc_python.bat
@@ -1,5 +1,5 @@
 @echo off
-python %~dp0\tools\ci_build\build.py --build_dir %~dp0\build\Windows %* --config Release --enable_pybind --build_wheel
+python %~dp0\tools\ci_build\build.py --build_dir %~dp0\build\Windows %* --config Release --build_wheel
 if not exist %~dp0build\env_doc_py virtualenv %~dp0build\env_doc_py --system-site-packages
 set PATH=%~dp0build\env_doc_py\Scripts;%PATH%
 if exist %~dp0build\env_doc_py\Scripts\pip3.7.exe pip install --force-reinstall %~dp0build\Windows\Release\Release\dist\lotus-0.1.4-cp37-cp37m-win_amd64.whl
