74801390d8eb00aab9e15efecb4db2b31cb6d564
xavier dupré <xavier.dupre@gmail.com>
2018-08-23 19:07:11 +0200

    Move instruction to build for the documentation in build_doc_python.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 rem Requires a python 3.6 or higher install to be available in your PATH
-python %~dp0\tools\ci_build\build.py --build_dir %~dp0\build\Windows %* --config Release --enable_pybind --build_wheel
+python %~dp0\tools\ci_build\build.py --build_dir %~dp0\build\Windows %*
