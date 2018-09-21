93d349bc9ea4f56e0224da1a3d3d4eeb3452deab
xavier dupré <xavier.dupre@gmail.com>
2018-09-21 16:24:55 +0200

    back to first option

--- onnxruntime/python/backend/backend.py
+++ onnxruntime/python/backend/backend.py
@@ -81,7 +81,7 @@ class OnnxRuntimeBackend(Backend):
         Compute the prediction.
         
         :param model: :class:`onnxruntime.InferenceSession` returned
-            by function *prepare* from module :mod:`onnxruntime.backend`
+            by function :func:`onnxruntime.backend.prepare`
         :param inputs: inputs
         :param device: requested device for the computation,
             None means the default one which depends on
