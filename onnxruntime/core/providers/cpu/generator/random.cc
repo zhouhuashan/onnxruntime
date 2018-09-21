c5fa077999271804a022f5e3eece643ea0564d78
Pranav Sharma <Pranav.Sharma@microsoft.com>
2018-09-21 06:56:02 +0000

    Merged PR 2793: Add additional copyright headers for tensorflow files as per legal guidance.
    Add additional copyright headers for tensorflow files as per legal guidance.

--- onnxruntime/core/providers/cpu/generator/random.cc
+++ onnxruntime/core/providers/cpu/generator/random.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 // NOTE: License applies to the multinomial implementation only.
+// Portions Copyright (c) Microsoft Corporation
+
 
 #include "core/providers/cpu/generator/random.h"
 
