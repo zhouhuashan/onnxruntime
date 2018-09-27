c192d042ef7c4720e17b4852bd70cfdbe9866e82
Ke Zhang <kezhan@microsoft.com>
2018-09-27 18:22:39 +0000

    Merged PR 2849: add history for mvn op.
    add history for mvn op.

--- onnxruntime/core/framework/environment.cc
+++ onnxruntime/core/framework/environment.cc
@@ -23,6 +23,36 @@ Status Environment::Initialize() {
       ONNX_NAMESPACE::OpSchemaRegistry::DomainToVersionRange::Instance().AddDomainToVersion(onnxruntime::kMSDomain, 1, 1);
     });
 
+	// Register MVN operator for backward compatibility.
+	// Experimental operator does not have history kept in ONNX. Unfortunately, RS5 takes bunch of experimental operators
+	// in onnx as production ops. MVN is one of them. Now (9/26/2018) MVN is a production function in ONNX. The experimental
+	// MVN op was removed. The history has to be kept locally as below.
+    LOTUS_ATTRIBUTE_UNUSED ONNX_OPERATOR_SCHEMA(MeanVarianceNormalization)
+        .SetDoc(R"DOC(Perform mean variance normalization.)DOC")
+        .Attr("across_channels", "If 1, mean and variance are computed across channels. Default is 0.", AttributeProto::INT, static_cast<int64_t>(0))
+        .Attr("normalize_variance", "If 0, normalize the mean only.  Default is 1.", AttributeProto::INT, static_cast<int64_t>(1))
+        .Input(0, "input", "Input tensor of shape [N,C,H,W]", "T")
+        .Output(0, "output", "Result, has same shape and type as input", "T")
+        .TypeConstraint(
+            "T",
+            {"tensor(float16)", "tensor(float)", "tensor(double)"},
+            "Constrain input and output types to float tensors.")
+        .TypeAndShapeInferenceFunction(propagateShapeAndTypeFromFirstInput);
+	// MVN operator is deprecated since operator set 9 (replaced with MVN function).
+    LOTUS_ATTRIBUTE_UNUSED ONNX_OPERATOR_SCHEMA(MeanVarianceNormalization)
+        .SetDoc(R"DOC(Perform mean variance normalization.)DOC")
+        .SinceVersion(9)
+        .Deprecate()
+        .Attr("across_channels", "If 1, mean and variance are computed across channels. Default is 0.", AttributeProto::INT, static_cast<int64_t>(0))
+        .Attr("normalize_variance", "If 0, normalize the mean only.  Default is 1.", AttributeProto::INT, static_cast<int64_t>(1))
+        .Input(0, "input", "Input tensor of shape [N,C,H,W]", "T")
+        .Output(0, "output", "Result, has same shape and type as input", "T")
+        .TypeConstraint(
+            "T",
+            {"tensor(float16)", "tensor(float)", "tensor(double)"},
+            "Constrain input and output types to float tensors.")
+        .TypeAndShapeInferenceFunction(propagateShapeAndTypeFromFirstInput);
+
     // Register MemCpy schema;
 
     // These ops are internal-only, so register outside of onnx
--- onnxruntime/test/providers/cpu/tensor/tensor_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/tensor_op_test.cc
@@ -430,7 +430,7 @@ void MeanVarianceNormalizationPerChannel(bool across_channels, bool normalize_va
   test.Run();
 }
 
-TEST(DISABLED_TensorOpTest, MeanVarianceNormalizationCPUTest) {
+TEST(TensorOpTest, MeanVarianceNormalizationCPUTest) {
   // across_channels: true, normalize_variance: true
   MeanVarianceNormalizationAcrossChannels(true, true);
 
