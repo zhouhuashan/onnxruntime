99197f5a3946a60896a6db841b188e28a1be2c83
xavier dupré <xavier.dupre@gmail.com>
2018-10-10 16:49:54 +0200

    update documentation with an example from onnx repo

--- docs/python/examples/plot_load_and_predict.py
+++ docs/python/examples/plot_load_and_predict.py
@@ -21,30 +21,37 @@ from onnxruntime.datasets import get_example
 
 #########################
 # Let's load a very simple model.
+# The model is available on github `onnx...test_sigmoid <https://github.com/onnx/onnx/tree/master/onnx/backend/test/data/node/test_sigmoid>`_.
 
-example1 = get_example("mul_1.pb")
+example1 = get_example("sigmoid.onnx")
 sess = rt.InferenceSession(example1)
 
 #########################
 # Let's see the input name and shape.
 
 input_name = sess.get_inputs()[0].name
-print(input_name)
+print("input name", input_name)
 input_shape = sess.get_inputs()[0].shape
-print(input_shape)
+print("input shape", input_shape)
+input_type = sess.get_inputs()[0].type
+print("input type", input_type)
 
 #########################
 # Let's see the output name and shape.
 
 output_name = sess.get_outputs()[0].name
-print(output_name)  
+print("output name", output_name)  
 output_shape = sess.get_outputs()[0].shape
-print(output_shape)
+print("output shape", output_shape)
+output_type = sess.get_outputs()[0].type
+print("output type", output_type)
 
 #########################
 # Let's compute its outputs (or predictions if it is a machine learned model).
 
-x = numpy.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=numpy.float32)
+import numpy.random
+x = numpy.random.random((3,4,5))
+x = x.astype(numpy.float32)
 res = sess.run([output_name], {input_name: x})
 print(res)
 
@@ -54,6 +61,9 @@ print(res)
 #
 # What happens if the dimension or the name are not right.
 # First, bad type.
+
+example2 = get_example("logreg_iris.onnx")
+sess = rt.InferenceSession(example2)
 
 try:
     x = numpy.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=numpy.float64)
