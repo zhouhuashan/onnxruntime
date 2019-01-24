6d318a3157f773d7202f86666e71b2233b195d9b
Ashwin Kumar <ashku@microsoft.com>
2019-01-24 12:58:57 -0800

    fix python error

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -294,7 +294,7 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
                  "-Donnxruntime_ENABLE_MICROSOFT_INTERNAL=" + ("ON" if args.enable_msinternal else "OFF"),
                  "-Donnxruntime_USE_BRAINSLICE=" + ("ON" if args.use_brainslice else "OFF"),
                  "-Donnxruntime_USE_NUPHAR=" + ("ON" if args.use_nuphar else "OFF"),
-                 "-Donnxruntime_USE_EIGEN_THREADPOOL="ON", 
+                 "-Donnxruntime_USE_EIGEN_THREADPOOL=ON", 
                  ]
     if args.use_brainslice:
         bs_pkg_name = args.brain_slice_package_name.split('.', 1)
